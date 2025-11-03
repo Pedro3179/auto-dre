ecd3688a0295343e4cc8fb7e2df4535882af5bcb
Leonardo Cerqueira <leonevescer@gmail.com>
2025-11-03 19:50:34 -0300

    Ask for file name

--- month/auto_dre.py
+++ month/auto_dre.py
@@ -1,33 +1,36 @@
 import pandas as pd
 
+#Ask for the file name.
+file_name=input('Enter the Daily Financial Report file (file.extension): ')
+
 #Ask for month and add it in the column month of RESUMO DESPESAS and RESUMO RECEITAS
 month=input('Enter the month: ')
 
 df_expenses=pd.read_excel(
-	'CONTROLE DIÁRIO - teste.xlsx', sheet_name='RESUMO DESPESAS'
+	file_name, sheet_name='RESUMO DESPESAS'
 )
 
 df_revenue=pd.read_excel(
-    'CONTROLE DIÁRIO - teste.xlsx', sheet_name='RESUMO RECEITAS'                         
+    file_name, sheet_name='RESUMO RECEITAS'                         
 )
 
 df_expenses['MES'], df_revenue['MES']=month, month
 
 with pd.ExcelWriter(						# File Handle to write on the file.
-	'CONTROLE DIÁRIO - teste.xlsx',         
+	file_name,         
 	engine='openpyxl',
-	mode='a',                              	# 'a' append: add things to the file.
+	mode='a',                              	# 'a' append: add data to the file.
 	if_sheet_exists='replace'              	# Substitutes the sheet if already exists.
 ) as writer:
     df_expenses.to_excel(writer, sheet_name='RESUMO DESPESAS', index=False)
     df_revenue.to_excel(writer, sheet_name='RESUMO RECEITAS', index=False)
 
 month_valE=pd.read_excel(
-	'CONTROLE DIÁRIO - teste.xlsx', sheet_name='RESUMO DESPESAS'
+	file_name, sheet_name='RESUMO DESPESAS'
 )
 
 month_valR=pd.read_excel(
-    'CONTROLE DIÁRIO - teste.xlsx', sheet_name='RESUMO RECEITAS'                         
+    file_name, sheet_name='RESUMO RECEITAS'                         
 )
 
 month_in_exp=month_valE['MES']
